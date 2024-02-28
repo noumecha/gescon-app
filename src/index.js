432811bf3c5edf3badb6be3256ad28366374405e
noumecha <noumechaivan@gmail.com>
2024-02-28 19:01:56 +0100

    maj : tmc

--- src/index.js
+++ src/index.js
@@ -6,23 +6,29 @@ import "assets/plugins/nucleo/css/nucleo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "assets/scss/argon-dashboard-react.scss";
 
+import { useAuth } from "services/AuthContext";
 import { AuthProvider } from "services/AuthContext";
 
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
 
 function App() {
-
+  const { isLoggedIn } = useAuth();
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/admin/*" element={<AdminLayout />} />
+        {isLoggedIn 
+          ?
+            (<Route path="/admin/index" element={<AdminLayout />} />)
+          :
+            (<Route path="/auth/*" element={<AuthLayout />} />)
+        }
+        {/*<Route path="/admin/*" element={<AdminLayout />} />
         <Route path="/auth/*" element={<AuthLayout />} />
-        <Route path="*" element={<Navigate to="/admin/index" replace />} />
+      <Route path="*" element={<Navigate to="/admin/index" replace />} />*/}
       </Routes>
     </BrowserRouter>
-  )
-
+  );
 }
 
 //root.render(<App />);
